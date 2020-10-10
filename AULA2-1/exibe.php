f7d80d27f4fa995f04a749c09a95370e58b2e3b5
chmaacedo <70985405+chmaacedo@users.noreply.github.com>
2020-10-10 18:29:09 -0300

    Back-end para form

---
+++ AULA2-1/exibe.php
@@ -0,0 +1,18 @@
+<html>
+
+<head>
+    <title>Exibição</title>
+</head>
+
+<body>
+    <h2>Hotel</h2>
+    <?php
+    $nome = $_GET["nomeusuario"];
+    $idade = $_GET["idadeusuario"];
+
+    echo "<p>Seu nome é: $nome</p>";
+    echo "<p>Sua idade é é: $idade</p";
+    ?>
+</body>
+
+</html>
