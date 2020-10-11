9667f381da2c96398af28946485a5b19bdff8cf8
chmaacedo <70985405+chmaacedo@users.noreply.github.com>
2020-10-11 09:06:00 -0300

    Recebendo dados do form

---
+++ AULA3/formulario.php
@@ -0,0 +1,25 @@
+<html>
+
+<head>
+    <title>Formulario</title>
+</head>
+
+<body>
+    <h1>
+        
+    </h1>
+
+
+    <?php
+
+    $nome = $_GET["nomeusuario"];
+    $sexo = $_GET["sexousuario"];
+    $email = $_GET["emailusuario"];
+    $datachegada = $_GET["data_chegada"];
+    $numnoite = $_GET["num_noite"];
+    $qtdpessoas = $_GET["qtd_pessoas"];
+
+    ?>
+</body>
+
+</html>
