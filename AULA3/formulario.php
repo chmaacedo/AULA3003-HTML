4556d9e0aa94718450e4069e555c3aef29e548e7
chmaacedo <70985405+chmaacedo@users.noreply.github.com>
2020-10-11 11:38:02 -0300

    Exemplo cadastro

--- AULA3/formulario.php
+++ AULA3/formulario.php
@@ -5,20 +5,25 @@
 </head>
 
 <body>
-    <h1>
-        
-    </h1>
-
-
     <?php
 
+    print_r($_GET); //Mostrando todas as variáveis puxando via GET
     $nome = $_GET["nomeusuario"];
-    $sexo = $_GET["sexousuario"];
-    $email = $_GET["emailusuario"];
     $datachegada = $_GET["data_chegada"];
     $numnoite = $_GET["num_noite"];
     $qtdpessoas = $_GET["qtd_pessoas"];
 
+    echo"<nav>
+        <h2>HOTEL PLAZA</h2>
+        <img src='https://media-cdn.tripadvisor.com/media/photo-s/16/1a/ea/54/hotel-presidente-4s.jpg' width='20%'
+            alt='Hotel'>
+        <hr>
+        <a href='index.html'>Inicio</a> |
+        <a href='cadastro.html'>Cadastro</a>
+        <hr>
+    </nav>";
+    echo "<p> Prezado(a) cliente $nome sua reserva foi concluída para $qtdpessoas hospedes.</p>";
+    echo "<p> Para o dia $datachegada, com previsão de saída $numnoite depois.</p>";
     ?>
 </body>
 
